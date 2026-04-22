0ac43397f7f158b9893cfa9e4374afaa9192baf8
amogh <amogh@amxgh.com>
2026-04-21 21:22:05 -0500

    feat: add EpochSequenceDataset to data loading utilities

--- sleep_classifier/data/__init__.py
+++ sleep_classifier/data/__init__.py
@@ -1,5 +1,17 @@
 """Data loading and preprocessing utilities."""
 
-from .epoch_dataset import PreparedDataBundle, PreparedSplitData, SleepWindowDataset, prepare_datasets
+from .epoch_dataset import (
+    EpochSequenceDataset,
+    PreparedDataBundle,
+    PreparedSplitData,
+    SleepWindowDataset,
+    prepare_datasets,
+)
 
-__all__ = ["PreparedDataBundle", "PreparedSplitData", "SleepWindowDataset", "prepare_datasets"]
+__all__ = [
+    "EpochSequenceDataset",
+    "PreparedDataBundle",
+    "PreparedSplitData",
+    "SleepWindowDataset",
+    "prepare_datasets",
+]
